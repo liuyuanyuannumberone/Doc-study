d6a4f4993a85bb99397af6a5976192dac5475a8e
liuyuanyuan <1445179059@qq.com>
2020-08-08 22:59:10 +0800

    20200808 数据库约束完毕

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -52,8 +52,7 @@ INSERT INTO `imc_course` VALUES
 (10,'MySQL课程-84741','通过学习MySQL课程-84741让你更有成就感!',1,5,2,'2018-05-25 04:24:47',1334,'00:00:00','','','',23,'',0.0,0.0,0.0,0.0);
 
 
-# 修改表结构
-ALTER TABLE imc_course ADD is_recommand TINYINT NOT NULL DEFAULT 0 COMMENT '是否推荐，0不推荐，1推荐';
+
 
 
 
@@ -112,11 +111,6 @@ INSERT INTO `imc_subsection` VALUES (1,1,1,'MySQL课程-79889第 1章第 1节','','mp4
 
 
 
-
-
-
-
-
 CREATE TABLE `imc_class` (
   `class_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '课程分类ID',
   `class_name` varchar(10) NOT NULL DEFAULT '' COMMENT '分类名称',
@@ -136,17 +130,13 @@ INSERT INTO `imc_class` VALUES (1,'MySQL','2019-01-24 08:57:26'),(2,'Redis','201
 -- 主键会自增、timestamp时间戳会自动拿到当前时间
 INSERT INTO imc_class (class_name) VALUES ('MySQL'),('Redis'),('MongoDB');
 
--- 建立索引
-CREATE UNIQUE INDEX uqx_classname  ON  imc_class (class_name);
 
--- 建立唯一索引后，执行INSERT INTO imc_class (class_name) VALUES ('MySQL');会报错
--- 但是我们想让索引重复(主键重复的)的数据其他字段得到更新。
+
+
+-- 我们想让索引重复(主键重复的)的数据其他字段得到更新。
 
 INSERT INTO imc_class (class_name) VALUES ('MySQL') ON DUPLICATE KEY UPDATE  add_time=CURRENT_TIME;
 --duplicate
-
-
-
 
 
 
@@ -228,19 +218,6 @@ INSERT INTO `imc_user` VALUES (1,'沿舟侨','7c345f726643d5a61753126d52b250d0','1'
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 CREATE TABLE `imc_question` (
   `quest_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '评论',
   `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
@@ -272,13 +249,6 @@ INSERT INTO `imc_question` VALUES (1,2314,57,727,5321,0,'用户:2314关于 SQLServer
 
 
 
-
-
-
-
-
-
-
 CREATE TABLE `imc_note` (
   `note_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '笔记ID',
   `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
@@ -290,10 +260,6 @@ CREATE TABLE `imc_note` (
   `add_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
   PRIMARY KEY (`note_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='笔记表';
-
-
-
-
 
 
 
@@ -324,14 +290,6 @@ INSERT INTO `imc_classvalue` VALUES (1,3065,8,9.0,9.1,8.1,0.0,'2019-01-28 17:10:
 
 
 
-
-
-
-
-
-
-
-
 CREATE TABLE `imc_selectcourse` (
   `select_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '选课ID',
   `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
@@ -350,7 +308,46 @@ INSERT INTO `imc_selectcourse` VALUES (1,1968,52,'2019-01-24 21:35:26','18:31:15
 
 
 
+
+
+/*
+数据备份
+
+蠕虫复制：创建一个跟现有表一样，然后将原表记录复制到新表
+create table bak.imc_class2020901 like imc_class      insert into  bak.imc_class2020901  select*from imc_class
+*/
+
+
+/*
+主键约束（唯一不能为null）：  ALTER TABLE imc_course ADD is_recommand TINYINT NOT NULL DEFAULT 0 COMMENT '是否推荐，0不推荐，1推荐';
+AUTO_INCREMENT  自动增长    插入数据时这个为null即可，数据库会自动增长     insert into student  values(null,'张三')
+
+
+唯一约束（UNIQUE)(唯一可以有null)
+索引  CREATE UNIQUE INDEX uqx_classname  ON  imc_class (class_name);
+
+非空约束  NOT NULL DEFAULT   插入数据时，默认值为default    insert into student  values(null,'张三'，default)
+
+外键约束： constraint  [外键约束名称] foreign key(外键名) references 主表名(主表某字段名)
+
+
+*/
+
+
+/*
+多表关系；
+
+一 对多： 一个用户下多个订单，一个订单只属于一个用户   （主表 从表（外键：与主表关联的键））
+          
+
+多对多：一个订单有多个商品，一个商品出现在不同的订单。   最好解决办法需要加表订单商品关系表
+        一个学生选多个课程，一个课程出现在多个学生中。
+*/
+
+ 
+
 -- ---------------------------------------------- SELECT------------------------------------------------
+# SELECT 执行顺序
 
 SELECT
      [ALL|DISTINCT|DISTINCTROW]
@@ -362,29 +359,42 @@ SELECT
      [ORDER BY]
      [LIMIT]
 
-
+sql的解析顺序： from  where   group by  聚合函数  having  select  order by  limit
 
 -- 下面是：比较运算符
 /*
-=  <  >  >=  <=  <>和!= 都表示不等于; BETWEEN 1000 AND 2000
+=  <  >  >=  <=  <>和!= 都表示不等于;
+
+*/
+
+
+/*
+  条件运算符
+BETWEEN 1000 AND 2000
 binary 区分大小写
-SELECT 'MYSQL' LIKE BINARY '_ysql'; 返回0
+NOT LIKE  %代表任意数量的字符， _代表一个字符
+IS NULL  IS NOT NULL   NOT IN
 
 */
 
 SELECT * FROM imc_class;
 SELECT class_id,class_name FROM imc_class;
-SELECT title FROM imc_course  WHERE title LIKE binary '%MYSQL%'; # 包含mysql的课程名   # NOT LIKE   %代表任意数量的字符， _代表一个字符
+SELECT title FROM imc_course  WHERE title LIKE binary '%MYSQL%'; # 包含mysql的课程名
+
 SELECT title,study_cnt FROM imc_course  WHERE study_cnt >= 5161;
+
 SELECT title,study_cnt FROM imc_course  WHERE study_cnt>=1000 AND study_cnt<=2000 ;#[1000-2000]
 SELECT title,study_cnt FROM imc_course  WHERE study_cnt BETWEEN 1000 AND 2000 ;#[1000-2000]
+
 SELECT * FROM imc_course WHERE title IS NOT NULL;     # IS NULL
+
 SELECT title FROM imc_course WHERE course_id IN (1,3,5);  #列的值在指定范围内的数据  NOT IN
+
 
 
 /*
 
--- 下面是：逻辑运算符 AND OR       && ||    XOR 一真一假返回真  2真2假返回假
+逻辑运算符 AND OR  NOT  && ||    XOR 一真一假返回真  2真2假返回假
 
 */
 
@@ -402,10 +412,114 @@ SELECT title,study_cnt FROM imc_course WHERE title LIKE '%mysql%' XOR study_cnt>
 任何运算符和NULL值运算结果都是NULL
 
 
+/*   排序：
+ORDER BY  对查询结果排序的最安全的方法   ASC升序（默认）  DCSC降序
+
+可以使用select子句中未出现的列或者是函数
+limit限制结果集的行数，常用于列表数据分页，前端分页，配合order by子句使用，为了保证每次获取到的数据按照同样的列排序
+limit 起始偏移量和行数    每页pageSize条，第currentPage页 （起始偏移量：(currentPage-1)*pageSize,行数：pageSize）
+
+*/
+SELECT * FROM student ORDER BY age ASC ,math DESC ;    # 多列排序 年龄升序，年龄相同，按分数降序排序。
+
+SELECT
+	course_id,
+	title
+FROM
+	imc_course
+ORDER BY                     #单列排序
+	study_cnt DESC
+LIMIT 0,10     #(currentPage-1)*pageSize,pageSize   如果最后一页没有10行，就会返回实际行数
+
+
+
+
+
 /*
+group by 将结果按照某些列分成不同的组，并对分组后的数据进行聚合操作
+having对聚合后的数据进行过滤。 where 是无法使用聚合函数
 
+-- 总结：1、一个返回一列，一个返回在一个值，必须使用group by聚合函数使用
+            select max(score), name from student ----ERROR 聚合查询结果只能包含聚合函数的结果，不能包含其他列，否则出现其他列的值也是无意义的
+	       2、select 列某,聚合函数  group by 列某
+
+常用的聚合函数:聚合函数也可以单独使用
+1、count(*)(col) 计算符合条件的数据行数(不包括null)   DISTINCT 去重   COUNT(DISTINCT user_id)
+2、sum(col_name) 计算符合条件的数值列的和
+3、avg(col_name) 计算符合条件的数值列的和的平均值
+4、max(col_name) 计算列的最大值  min(col_name)
+*/
+
+SELECT COUNT(*) FROM imc_course; #10行
+SELECT COUNT(DISTINCT user_id) FROM imc_course; # 去重
+SELECT SUM(study_cnt) FROM  imc_course;
+
+
+SELECT
+	level_name,
+	count( * )
+FROM
+	imc_course a
+	JOIN imc_level b ON a.level_id = b.level_id
+GROUP BY
+	level_name
+
+-- 入门  5
+-- 中级  4
+-- 高级  6
+
+SELECT
+	class_name,
+	level_name,
+	count( * )
+FROM
+	imc_course a
+	JOIN imc_class b ON b.class_id = a.class_id
+	JOIN imc_level c ON c.level_id = a.level_id          #把几个表连接起来
+GROUP BY
+	class_name,
+	level_name
+HAVING
+	count( * ) >3
+
+
+
+	#参考1                                                                   #参考2
+SELECT                                                                 SELECT
+	course_id,                   #返回一列                                        course_id,
+	AVG( content_score ) AS avg_content,  #返回一个值                               (SELECT AVG(content_score) FROM  imc_classvalue)  #数值
+	AVG( level_score ) AS avg_level,               <= 区分 =>              FROM
+	AVG( logic_score ) AS avg_logic                                       imc_classvalue
+FROM
+	imc_classvalue
+GROUP BY
+	course_id;
+
+
+#参考3
+
+SELECT
+	(SELECT AVG(content_score) FROM  imc_classvalue )  #数值
+FROM
+	imc_classvalue
+
+
+
+ #参考四
+
+SELECT
+	title,( SELECT MAX( study_cnt ) FROM imc_course ) #数值
+FROM
+	imc_course
+WHERE
+	study_cnt = (SELECT MAX( study_cnt ) FROM imc_course ) #数值
+
+
+
+
+
+/*
 多个表中查询数据--关联查询
-
 JOIN
 INNER JOIN 内关联（A&&B）     select <select list>  from  tableA A INNER JOIN tableB B   ON A.key=B.key;  (两个表的集合)
 OUTER JOIN 外关联   LEFT  JOIN (左连接)  和  RIGHT JOIN（右连接）
@@ -457,117 +571,6 @@ LEFT 	JOIN imc_chapter b ON a.course_id = b.course_id   WHERE b.chapter_no IS NU
 
 
 
-/*
-group by 将结果按照某些列分成不同的组，并对分组后的数据进行聚合操作
-
-where 是无法使用聚合函数，所以使用having，对聚合后的数据进行过滤。
-
--- 总结：1、数值可以作为查询列(select 5 from imc_class);
-	       2、一个返回一列，一个返回在一个值，必须使用group by ==列和聚合函数使用，必须使用group by;
-	       3、select   列某+聚合函数  group by 列某  （列某=列某）
-
-常用的聚合函数:聚合函数也可以单独使用
-
-1、count(*)(col) 计算符合条件的数据行数
-2、sum(col_name) 计算符合条件的数值列的和
-3、avg(col_name) 计算符合条件的数值列的和的平均值
-4、max(col_name) 计算列的最大值  min(col_name)
-
-
-SELECT COUNT(*) FROM imc_course; #10行
-SELECT COUNT(DISTINCT user_id) FROM imc_course; # 去重
-
-SELECT SUM(study_cnt) FROM  imc_course;
-
-
-
-*/
-
-
-SELECT
-	level_name,
-	count( * )
-FROM
-	imc_course a
-	JOIN imc_level b ON a.level_id = b.level_id
-GROUP BY
-	level_name
-
--- 入门  5
--- 中级  4
--- 高级  6
-
-
-SELECT
-	class_name,
-	level_name,
-	count( * )
-FROM
-	imc_course a
-	JOIN imc_class b ON b.class_id = a.class_id
-	JOIN imc_level c ON c.level_id = a.level_id          #把几个表连接起来
-GROUP BY
-	class_name,
-	level_name
-HAVING
-	count( * ) >3
-
-
-	#参考1                                                                   #参考2
-SELECT                                                                 SELECT
-	course_id,                   #返回一列                                        course_id,
-	AVG( content_score ) AS avg_content,  #返回一个值                               (SELECT AVG(content_score) FROM  imc_classvalue)  #数值
-	AVG( level_score ) AS avg_level,               <= 区分 =>              FROM
-	AVG( logic_score ) AS avg_logic                                       imc_classvalue
-FROM
-	imc_classvalue
-GROUP BY
-	course_id;
-
-
-#参考3
-
-SELECT
-	(SELECT AVG(content_score) FROM  imc_classvalue )  #数值
-FROM
-	imc_classvalue
-
-
-
- #参考四
-
-SELECT
-	title,( SELECT MAX( study_cnt ) FROM imc_course ) #数值
-FROM
-	imc_course
-WHERE
-	study_cnt = (SELECT MAX( study_cnt ) FROM imc_course ) #数值
-
-
-
-
-
-
-/*
-ORDER BY  对查询结果排序的最安全的方法   ASC升序  DCSC降序
-可以使用select子句中未出现的列或者是函数
-
-limit限制结果集的行数，常用于列表数据分页，前端分页，配合order by子句使用，为了保证每次获取到的数据按照同样的列排序
-limit 起始偏移量和行数    每页pageSize条，第currentPage页 （起始偏移量：(currentPage-1)*pageSize,行数：pageSize）
-
-*/
-
-
-SELECT
-	course_id,
-	title 
-FROM
-	imc_course 
-ORDER BY
-	study_cnt DESC 
-LIMIT 0,10     #(currentPage-1)*pageSize,pageSize   如果最后一页没有10行，就会返回实际行数
-
-
 
 
 
@@ -580,14 +583,14 @@ LIMIT 0,10     #(currentPage-1)*pageSize,pageSize   如果最后一页没有10行，就会返
 -- ----------------------------------------------------删除/更新---------------------------------------------
 
 /*
-DELETE 
+DELETE
 FROM table_name
 [where]
 [order by]
 [limit](这里只写一个参数）
 */
 
-SELECT 
+SELECT
 *
 FROM
 	imc_course a
@@ -603,22 +606,23 @@ WHERE b.course_id IS NULL
 
 
 
-DELETE a 
+DELETE a
 FROM
 	imc_type a
 	JOIN (
 SELECT
 	type_name,
 	MIN( type_id ) AS min_type_id,
-	count( * ) 
+	count( * )
 FROM
-	imc_type 
+	imc_type
 GROUP BY
-	type_name 
+	type_name
 HAVING
-	count( * ) > 1 
-	) b ON a.type_name = b.type_name 	AND a.type_id > min_type_id;
-	
+	count( * ) > 1
+	) b ON a.type_name = b.type_name
+ 	AND a.type_id > min_type_id;
+
 
 
 
@@ -627,19 +631,16 @@ HAVING
 更新
 */
 
-
-UPDATE imc_user 
-SET user_status = 0 
-WHERE
-	user_nick = '侯鹿焦'
+UPDATE imc_user
+SET user_status = 0
+WHERE  user_nick = '侯鹿焦'
 
 
 UPDATE 	imc_course
 SET is_recommand=1
 ORDER BY
-	RAND( ) 
+	RAND( )
 LIMIT 10
-
 
 
 
@@ -649,12 +650,12 @@ SELECT
 	course_id,
 	AVG( content_score ) AS avg_content,
 	AVG( level_score ) AS avg_level,
-	AVG( logic_score ) AS avg_logic 
+	AVG( logic_score ) AS avg_logic
 FROM
-	imc_classvalue 
+	imc_classvalue
 GROUP BY
-	course_id 
-	) b ON a.course_id = b.course_id 
+	course_id
+	) b ON a.course_id = b.course_id
 	SET a.content_score = b.avg_content,
 	a.level_score = b.avg_level,
 	a.logic_score = b.avg_logic
@@ -698,7 +699,7 @@ a.course_id,
 a.title,
 b.class_name,
 c.type_name,
-d.level_name 
+d.level_name
 FROM
 	imc_course a
 	JOIN imc_class b ON a.class_id = b.class_id
@@ -722,7 +723,7 @@ SELECT
 	CURTIME( ), #09:13:03
 	NOW( ); # 2020-06-20 09:13:03
   DATE_FORMAT (date,fmt) # 按照fmt格式，对date进行格式化 %Y(四位的年) %m（00-12） %d（00-31）  %H （00-24） %i（00-59） %s（00-50）
-  
+
 SELECT   DATE_FORMAT(now(),'%Y年%m月%d日 %H:%i:%s')  AS time;
 
 
@@ -740,12 +741,12 @@ SELECT
 	DATEDIFF(now(), online_time )  AS   diff_time         #返回两个日期之间相差的天数
 FROM
 	imc_course;
-	
 
-/* 
+
+/*
 minutes
 DATE_ADD(date,INTERVAL YEAR|DAY|HOUR|MINUTES|SECOND) 给定的日期增加或者减少指定的时间单元
-*/ 
+*/
 
 SELECT
 	NOW( ),
@@ -760,12 +761,12 @@ EXTRACT(unit from date) 返回日期date的指定部分
 SELECT
 	NOW( ),
 	EXTRACT(YEAR FROM NOW())  #提取时间年份
-	
-	
+
+
 /*
 时间戳
 
-TIMESTAMP   CURRENT_TIMESTAMP    
+TIMESTAMP   CURRENT_TIMESTAMP
 
 UNIX_TIMESTAMP() #unix时间戳，返回的是秒数
 
@@ -774,7 +775,7 @@ UNIX_TIMESTAMP() #unix时间戳，返回的是秒数
 SELECT
 	NOW( ),
 	UNIX_TIMESTAMP()  #1592632749
-	
+
 -- ------------------------------------------------/时间函数---------------------------------------------------
 
 
@@ -805,7 +806,7 @@ SELECT  FORMAT(12345.7,2)  # 保留4位小数 12,345.70
 
 */
 
-SELECT 
+SELECT
   LEFT	( 'hello,word', 5 ),# hello
 	RIGHT ( 'hello,word', 5 ),# .word
 	SUBSTRING( 'hello,word', 2 ),# ello,word
@@ -822,13 +823,13 @@ SELECT
 	title,
 	LOCATE( '-', title ),
 	SUBSTRING( title, 1, LOCATE( '-', title ) - 1 ),
-	SUBSTRING_INDEX( title, '-', 1 ) 
+	SUBSTRING_INDEX( title, '-', 1 )
 FROM
 	imc_course
 -- ------------------------------------------------/字符串函数---------------------------------------------------
 
 SELECT ROUND(122.456,2)   # 122.46 对数值四舍五入保留2位小数
-SELECT RAND()    # 返回0-1之间的随机数 
+SELECT RAND()    # 返回0-1之间的随机数
 SELECT MD5('323232323')  #加密
 
 
@@ -836,17 +837,17 @@ SELECT
 	user_nick,
 	sex,
 CASE
-	WHEN sex = 1 THEN '男' 
-	WHEN sex = 0 THEN '女' 
-	ELSE '未知' 
-	END   AS '性别' 
+	WHEN sex = 1 THEN '男'
+	WHEN sex = 0 THEN '女'
+	ELSE '未知'
+	END   AS '性别'
 FROM
-	imc_user 
+	imc_user
 WHERE
-CASE	
-		WHEN sex = 1 THEN  '男' 
+CASE
+		WHEN sex = 1 THEN  '男'
 		WHEN sex = 0 THEN '女'
-		ELSE '未知' 
+		ELSE '未知'
 END = '男'
 
 -- --------------------------------------------------/其他函数------------------------------------------------
@@ -860,14 +861,14 @@ CTE生成一个命名临时表，并且只在查询期间有效;
 CTE临时表在一个查询中可以多次引用以及自引用;
 */
 
-WITH cte AS ( SELECT title, study_cnt, class_id FROM imc_course WHERE study_cnt > 2000 ) 
+WITH cte AS ( SELECT title, study_cnt, class_id FROM imc_course WHERE study_cnt > 2000 )
 SELECT  *  FROM   cte
 
 
 
 -- CTE递归生成序列
 WITH RECURSIVE test  AS ( SELECT 1 AS n UNION ALL SELECT 1+n FROM  test WHERE  n<10)
-SELECT * FROM test 
+SELECT * FROM test
 
 
 WITH RECURSIVE replay ( quest_id, quest_title, user_id, replyid, path ) AS (
@@ -880,7 +881,7 @@ SELECT
 FROM
 	imc_question
 WHERE
-	course_id = 59 AND replyid = 0 
+	course_id = 59 AND replyid = 0
 UNION ALL
 SELECT
   a.quest_id,
@@ -890,8 +891,8 @@ SELECT
 	CONCAT(b.path,'>>',a.quest_id) AS path
 FROM
 	imc_question a
-	JOIN  replay b ON a.replyid = b.quest_id 
-	)	
+	JOIN  replay b ON a.replyid = b.quest_id
+	)
 	SELECT * FROM replay
 
 -- ---------------------------------------------------/公共表------------表达式(CTE)----------------------------------------------
@@ -907,21 +908,21 @@ DENSE_RANK() L类似于rank，当组内某行数据重复时，虽然行号会重复，但后续的行号不会
 
 
 WITH test (study_name,class_name,score) AS (
-SELECT 'tom','mysql','98' 
+SELECT 'tom','mysql','98'
 UNION ALL
-SELECT 'jrm','mysql','98' 
+SELECT 'jrm','mysql','98'
 UNION ALL
-SELECT 'jss','mysql','98' 
+SELECT 'jss','mysql','98'
 UNION ALL
-SELECT 'jerry','mysql','58' 
+SELECT 'jerry','mysql','58'
 UNION ALL
-SELECT 'moimi','redis','48' 
+SELECT 'moimi','redis','48'
 UNION ALL
-SELECT 'ghh','redis','18' 
+SELECT 'ghh','redis','18'
 UNION ALL
-SELECT 'mike','redis','98' 
+SELECT 'mike','redis','98'
 UNION ALL
-SELECT 'srri','redis','68' 
+SELECT 'srri','redis','68'
 )
 #partition子句：窗口按照那些字段进行分组，窗口函数在不同的分组上分别执行。
 SELECT  study_name,class_name, score,
@@ -938,7 +939,7 @@ SELECT
 	study_cnt,
 	title,
 	score,
-	RANK ( ) OVER ( PARTITION BY class_name ORDER BY score DESC ) AS rk 
+	RANK ( ) OVER ( PARTITION BY class_name ORDER BY score DESC ) AS rk
 FROM
 	imc_course a
 	JOIN imc_class b ON a.class_id = b.class_id
@@ -977,16 +978,16 @@ SELECT
 	b.class_id
 FROM
 	imc_course a
-	JOIN imc_class b ON a.class_id = b.class_id 
+	JOIN imc_class b ON a.class_id = b.class_id
 # AND a.class_id = 5   #left join 返回结果错误
   WHERE  a.class_id = 5
- 
+
 -- 4、在使用in进行子查询的判断时，在列中未指定正确的表名，所以只当最好的办法关联代替子查询.
 SELECT A1 FROM	A  WHERE A1 IN  (SELECT A1 FROM B),#这时尽管B中不存在A1列，数据库也不会报错，而是会列出A表中所有的数据。错误的
 # B.A1  正确
 
 SELECT title FROM imc_course WHERE title  IN (SELECT imc_class.title FROM imc_class); # 正确的
-SELECT title FROM imc_course WHERE course_id IN (1,3,5);  #列的值在指定范围内的数据  NOT IN 
+SELECT title FROM imc_course WHERE course_id IN (1,3,5);  #列的值在指定范围内的数据  NOT IN
 --  最好的办法关联代替子查询
 
 SELECT a.title FROM imc_course a
